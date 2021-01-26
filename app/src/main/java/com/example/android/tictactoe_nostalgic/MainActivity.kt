2ed8e734cd1c4b90db67fcc3c71dff443b29ac0c
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2021-01-26 22:11:01 +0530

    Front End Finalised (Few Bugs remaining)

--- app/src/main/java/com/example/android/tictactoe_nostalgic/MainActivity.kt
+++ app/src/main/java/com/example/android/tictactoe_nostalgic/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.android.tictactoe_nostalgic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var turn : Int=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView1.setOnClickListener(this)
+        imageView2.setOnClickListener(this)
+        imageView3.setOnClickListener(this)
+        imageView4.setOnClickListener(this)
+        imageView5.setOnClickListener(this)
+        imageView6.setOnClickListener(this)
+        imageView7.setOnClickListener(this)
+        imageView8.setOnClickListener(this)
+        imageView9.setOnClickListener(this)
+
+    }
+
+
+    override fun onClick(v: View?) {
+        if(turn%2==0){
+            v?.setBackgroundResource(R.drawable.x)
+            turn++
+        }else{
+            v?.setBackgroundResource(R.drawable.o)
+            turn++
+        }
     }
 }
